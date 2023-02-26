99e7265604e2400f6af05e7639bedabc911be48b
Tr1NgleDev <steamkyzagames2@gmail.com>
2023-02-26 15:37:58 +0800

    h

--- README.md
+++ README.md
@@ -1,10 +1,2 @@
-# 4DMod Example
- This is a project example of mod for the [4D Modding](https://gdpseditor.com/4dmodding/ "4D Modding") framework.
- 
- For basic tutorials you should check [4dmodding-tutorials](https://github.com/4D-Modding/4dmodding-tutorials "4dmodding-tutorials") repo.
-
- You can also check out [4DFly Source-Code](https://github.com/4D-Modding/4DFly).
-
- Also if you are going to use this 4DMod Example, use it as repo template instead of just `git clone` it 
-
- idk what else to put there lol
+# Speed-Hack
+ This is a basic speed-hack mod for 4D Miner using the 4D Modding framework.
--- main.cpp
+++ main.cpp
@@ -1,39 +1,89 @@
 //#define DEBUG_CONSOLE // Uncomment this if you want a debug console
 
 // Mod Name. Make sure it matches the mod folder's name
-#define MOD_NAME "4DMod"
+#define MOD_NAME "Speed-Hack"
 #define MOD_VER "1.0"
 
 #include <Windows.h>
 #include <cstdio>
 #include <4dm.h>
 using namespace fdm;
+using namespace fdm::gui;
 
+float speed = 1.0;
+bool g = false;
 
-void(__thiscall* GameState_init)(GameState* self, StateManager& s);
-void __fastcall GameState_init_H(GameState* self, StateManager& s)
-{
-	glfwInit(); // for glfw functions to work
-
-	// Your code that runs at first frame here (it calls when you load into the world)
-
-	GameState_init(self, s);
-}
+FontRenderer* speedText;
 
 void(__thiscall* Player_update)(Player* self, GLFWwindow* window, World* world, double dt);
 void __fastcall Player_update_H(Player* self, GLFWwindow* window, World* world, double dt) 
 {
-	// Your code that runs every frame here (it only calls when you play in world, because its Player's function)
-
-	Player_update(self, window, world, dt);
+	Player_update(self, window, world, dt * speed);
 }
 
 bool(__thiscall* Player_keyInput)(Player* self, GLFWwindow* window, int key, int scancode, int action, int mods);
 bool __fastcall Player_keyInput_H(Player* self, GLFWwindow* window, int key, int scancode, int action, int mods) 
 {
-	// Your code that runs when Key Input happens (check GLFW Keyboard Input tutorials)|(it only calls when you play in world, because its Player's function)
-
+	if (key == GLFW_KEY_G) 
+	{
+		if (action == GLFW_PRESS) g = true;
+		else if (action == GLFW_RELEASE) g = false;
+	}
+	
 	return Player_keyInput(self, window, key, scancode, action, mods);
+}
+
+void(__thiscall* GameState_scrollInput)(GameState* self, StateManager& s, double xOff, double yOff);
+void __fastcall GameState_scrollInput_H(GameState* self, StateManager& s, double xOff, double yOff)
+{
+	GameState_scrollInput(self, s, xOff, yOff);
+	if (g)
+	{
+		self->player.angleToRotate -= yOff * self->player.scrollFactor * 0.0099999998f;
+		speed += yOff * 0.05f;
+		speed = glm::clamp(speed, 0.05f, 10.0f);
+	}
+}
+
+void(__thiscall* GameState_mouseButtonInput)(GameState* self, StateManager& s, int button, int action, int mods);
+void __fastcall GameState_mouseButtonInput_H(GameState* self, StateManager& s, int button, int action, int mods)
+{
+	if (button == GLFW_MOUSE_BUTTON_MIDDLE && action == GLFW_PRESS && g) speed = 1.0;
+	GameState_mouseButtonInput(self, s, button, action, mods);
+}
+std::stringstream str;
+void(__thiscall* Player_renderHud)(Player* self, GLFWwindow* window);
+void __fastcall Player_renderHud_H(Player* self, GLFWwindow* window)
+{
+	Player_renderHud(self, window);
+	str.str(std::string());
+	str << "Speed:" << std::setprecision(3) << speed;
+	speedText->setText(str.str());
+	speedText->updateModel();
+	speedText->render();
+}
+const Shader* fontShader;
+const Tex2D* fontTex;
+
+void(__thiscall* GameState_init)(GameState* self, StateManager& s);
+void __fastcall GameState_init_H(GameState* self, StateManager& s)
+{
+	glfwInit();
+
+	GameState_init(self, s);
+
+	int width, height;
+	glfwGetWindowSize(s.window, &width, &height);
+
+	fontTex = ResourceManager::get("pixelfont.png");
+	fontShader = ShaderManager::get("textShader");
+
+	speedText = new FontRenderer(fontTex, fontShader);
+	speedText->centered = true;
+	speedText->color = glm::vec4{ 1.0f };
+	speedText->charSize = glm::ivec2{ 8, 8 };
+	speedText->fontSize = 2;
+	speedText->pos = glm::ivec2{ width / 2.0f, 25 };
 }
 
 DWORD WINAPI Main_Thread(void* hModule)
@@ -44,15 +94,14 @@ DWORD WINAPI Main_Thread(void* hModule)
 	FILE* fp;
 	freopen_s(&fp, "CONOUT$", "w", stdout);
 #endif
-	// Hook to the GameState::init function
-	Hook(reinterpret_cast<void*>(base + idaOffsetFix(0x50690)), reinterpret_cast<void*>(&GameState_init_H), reinterpret_cast<void**>(&GameState_init));
-
-	// Hook to the Player::update function
 	Hook(reinterpret_cast<void*>(base + idaOffsetFix(0x7EB40)), reinterpret_cast<void*>(&Player_update_H), reinterpret_cast<void**>(&Player_update));
-
-	// Hook to the Player::keyInput function
 	Hook(reinterpret_cast<void*>(base + idaOffsetFix(0x81880)), reinterpret_cast<void*>(&Player_keyInput_H), reinterpret_cast<void**>(&Player_keyInput));
-
+	Hook(reinterpret_cast<void*>(base + idaOffsetFix(0x53770)), reinterpret_cast<void*>(&GameState_scrollInput_H), reinterpret_cast<void**>(&GameState_scrollInput));
+	Hook(reinterpret_cast<void*>(base + idaOffsetFix(0x53870)), reinterpret_cast<void*>(&GameState_mouseButtonInput_H), reinterpret_cast<void**>(&GameState_mouseButtonInput));
+	
+	Hook(reinterpret_cast<void*>(base + idaOffsetFix(0x7C6A0)), reinterpret_cast<void*>(&Player_renderHud_H), reinterpret_cast<void**>(&Player_renderHud));
+	Hook(reinterpret_cast<void*>(base + idaOffsetFix(0x50690)), reinterpret_cast<void*>(&GameState_init_H), reinterpret_cast<void**>(&GameState_init));
+	
 	EnableHook(0);
 	return true;
 }
